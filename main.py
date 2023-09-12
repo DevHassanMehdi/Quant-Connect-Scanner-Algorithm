c897e716c9cf78c17a92dc99f60ca04bb00e131f
hassanmehdi <hassanmehdi510@gmail.com>
2023-09-13 00:19:27 +0500

    Updated preliminaries

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class ScannerAlgorithm(QCAlgorithm):
 	
 	def Initialize(self):
 		# Setting the start and end dates for the backtest
-		self.SetStartDate(self.Time - timedelta(days=30))
+		self.SetStartDate(self.Time - timedelta(hours=1))
 		# Setting the initial cash available for trading
 		self.SetCash(100000)
 		
@@ -26,6 +26,10 @@ class ScannerAlgorithm(QCAlgorithm):
 		self.max_market_cap = 6
 		self.pr = {}
 		self.volume_last_second = {}
+		
+		# Set a timer for 300 seconds (5 minutes) to stop the algorithm
+		self.L = timedelta(seconds=300)
+		self.timer = self.Time + self.L
 		
 		# Schedule the Trade function to run every day at 11:30 AM NYT
 		# self.Schedule.On(self.DateRules.EveryDay(), self.TimeRules.At(11, 30), self.Trade)
@@ -63,60 +67,59 @@ class ScannerAlgorithm(QCAlgorithm):
 	
 	def Trade(self):
 		self.Debug("Trade called")
-		
-		# # Getting the current time
-		# current_time = self.Time
-		# # If the current time is outside the specified interval, do nothing and return
-		# if current_time.time() < datetime.time(self.min_hour, self.min_minute) or current_time.time() > datetime.time(
-		# 		self.max_hour, self.max_minute):
-		# 	return
-		
-		# # Getting the elapsed time
-		# elapsed_time = (datetime.now() - self.start_time).total_seconds()
-		# # If the elapsed time is less than the Tr value, do nothing and return
-		# if elapsed_time < self.Tr:
-		# 	return
-		#
-		
-		# Looping through each symbol in the list of symbols
+		# Loop through each symbol in the list of symbols
 		for security in self.ActiveSecurities.Values:
 			symbol = security.Symbol
 			self.Debug(f"Trade symbol: {symbol}")
-			self.pr[symbol] = 0
-			self.volume_last_second[symbol] = 0
 			
-			# Getting the current trade bar for the symbol
+			# Get the current trade bar for the symbol
 			trade_bar = self.ActiveSecurities[symbol]
-			# Checking if the trade bar is not None
+			
+			# Check if the trade bar is not None
 			if trade_bar is None:
 				continue
 			
-			# Getting the current market price for the symbol
+			# Get the current market price for the symbol (P)
 			current_price = trade_bar.Close
 			
-			# Getting the historic volume for the symbol
+			# Get the historic volume for the last hour (Hvol)
 			history = self.History(symbol, 60, Resolution.Minute)
 			try:
 				historic_volume = history.loc[symbol].iloc[-1]['volume']
-				fundamental = security.Fundamentals
-				market_cap = fundamental.MarketCap
-				# Calculating the new market cap for the symbol
-				new_market_cap = round((market_cap / 1000000000) ** 0.5, 2)
-				
-				# Getting the volume for the current second
-				current_second = self.Time.second
-				volume_this_second = self.ActiveSecurities[symbol].Volume - self.volume_last_second[symbol]
-				self.volume_last_second[symbol] = self.ActiveSecurities[symbol].Volume
-				
-				# Getting the exact time interval of the last "second"
-				exact_time_interval = self.Time - timedelta(seconds=current_second)
-				
-				# Printing the preliminary values for the symbol
-				self.Debug(
-					f"Symbol: {symbol}, Pr: {self.pr[symbol]}, Hvol: {historic_volume}, P: {current_price}, Market Cap: {market_cap}, NMC: {new_market_cap}, Vsec: {volume_this_second}, Vt: {exact_time_interval}")
-			
 			except KeyError:
 				self.Debug(f"Historic data not available for {symbol}")
+				historic_volume = 0
+			
+			# Get the current market cap (MarketCap)
+			fundamental = security.Fundamentals
+			market_cap = fundamental.MarketCap
+			
+			# Calculate the new market cap (NMC)
+			new_market_cap = round((market_cap / 1_000_000_000) ** 0.5, 2)
+			
+			# Get the volume for the current second (Vsec)
+			current_second = self.Time.second
+			volume_this_second = self.ActiveSecurities[symbol].Volume - self.volume_last_second.get(symbol, 0)
+			self.volume_last_second[symbol] = self.ActiveSecurities[symbol].Volume
+			
+			# Get the exact time interval of the last "second" (Vt)
+			exact_time_interval = self.Time - timedelta(seconds=current_second)
+			
+			# Calculate Pr based on your reference time (e.g., 11:30 AM)
+			reference_time = self.Time.replace(hour=11, minute=30, second=0, microsecond=0)
+			if self.Time == reference_time:
+				pr = current_price
+			else:
+				pr = self.pr.get(symbol, 0)  # Use the previous Pr if not at reference time
+			
+			# Store the current Pr for the next iteration
+			self.pr[symbol] = current_price
+			
+			# Now you have all the required information for each stock
+			self.Debug(
+				f"Symbol: {symbol}, Pr: {pr}, Hvol: {historic_volume}, P: {current_price}, "
+				f"Market Cap: {market_cap}, NMC: {new_market_cap}, Vsec: {volume_this_second}, Vt: {exact_time_interval}"
+			)
 # symbol = security.Symbol
 # self.Debug(f"Trade symbol: {symbol}")
 #
