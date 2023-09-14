b1796a3d1c2831bc1e3cdf51cb11dc2ab476e1ed
hassanmehdi <hassanmehdi510@gmail.com>
2023-09-15 00:53:40 +0500

    Added code for Interium and Final calculations. Also added code for Thresholds and triggers

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ class ScannerAlgorithm(QCAlgorithm):
 	
 	def Initialize(self):
 		# Setting the start and end dates for the backtest
-		self.SetStartDate(self.Time - timedelta(hours=1))
-		# Setting the initial cash available for trading
+		self.SetStartDate(2023, 9, 12)  # set the start date to 60 minutes ago
 		self.SetCash(100000)
 		self.SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Cash)
 		
@@ -28,6 +27,7 @@ class ScannerAlgorithm(QCAlgorithm):
 		self.min_market_cap = 3
 		self.max_market_cap = 6
 		self.min_descent = 10
+		self.power_factor = 0.5
 		
 		# Define variables
 		self.stock = None
@@ -37,6 +37,7 @@ class ScannerAlgorithm(QCAlgorithm):
 		self.MarketCap = None
 		self.NMC = None
 		self.Vsec = None
+		self.V = None
 		self.Vt = None
 		self.Vmin = None
 		self.VPnow = None
@@ -60,69 +61,75 @@ class ScannerAlgorithm(QCAlgorithm):
 		
 		# Schedule function to run every second
 		self.Schedule.On(self.DateRules.EveryDay(),
-						 self.TimeRules.Every(timedelta(seconds=1)),
+						 self.TimeRules.Every(timedelta(minutes=1)),
 						 self.ScanRussell3000)
 	
 	# Defining the CoarseSelectionFunction method that takes a coarse universe of symbols as input and returns a list of symbols
 	def CoarseSelectionFunction(self, coarse):
-		# Filter the universe of stocks
-		sortedByDollarVolume = sorted(coarse, key=lambda x: x.DollarVolume, reverse=True)
-		coarse_objetcs = [x.Symbol for x in sortedByDollarVolume if
-						  x.HasFundamentalData and
-						  x.DollarVolume >= 100000]
-		self.Debug(f"Coarse objects: {len(coarse_objetcs)}")
-		# Returning the filtered symbols as a list
-		return coarse_objetcs
+		# Filter the coarse universe to include only US equities with market cap between 3- billion
+		filtered = [x for x in coarse if
+					x.HasFundamentalData and
+					x.Price > 5 and
+					x.DollarVolume > 1000000 and
+					x.Symbol.SecurityType == SecurityType.Equity and
+					x.Symbol.ID.Market == Market.USA]
+		# Sort the filtered universe by dollar volume in descending order
+		sortedByDollarVolume = sorted(filtered, key=lambda x: x.DollarVolume, reverse=True)
+		# Return the top 100 securities by dollar volume
+		coarse_objects = [x.Symbol for x in sortedByDollarVolume]
+		self.Debug(f"Coarse objects: {len(coarse_objects)}")
+		return coarse_objects
 	
 	def FineSelectionFunction(self, fine):
 		# Fine Filter the universe of stocks
 		fine_objects = [x.Symbol for x in fine if
 						x.MarketCap >= self.min_market_cap * 1000000000 and
-						x.MarketCap <= self.max_market_cap * 10000000000]
+						x.MarketCap <= self.max_market_cap * 1000000000]
 		self.Debug(f"fine objects: {len(fine_objects)}")
-		
-		# Return the fine filtered symbols
 		return fine_objects
 	
 	def ScanRussell3000(self):
+		self.Debug(f"Scanning US Equities Russel3000")
 		# Loop through each symbol in the list of symbols
 		for security in self.ActiveSecurities.Values:
 			symbol = security.Symbol
-			
 			# Get the current trade bar for the symbol
 			trade_bar = self.ActiveSecurities[symbol]
 			if trade_bar is None:
 				continue
 			
-			# Check if we are in the warm-up period
-			if self.IsWarmingUp:
-				self.Debug("Still warming up, data may not be available.")
-				return
-			
 			# Get the current market price for the symbol (P)
 			self.P = trade_bar.Close
-			
+			if not self.P > 0:
+				continue
+			self.V = trade_bar.Volume
+			if not self.V > 0:
+				continue
 			# Get the historic volume for the last hour (Hvol)
 			history = self.History(symbol, self.LM, Resolution.Minute)
 			try:
 				self.Hvol = history.loc[symbol].iloc[-1]['volume']
 			except KeyError:
 				self.Debug(f"Historic data not available for {symbol}")
+				continue
 			
 			# Get the current market cap (MarketCap)
 			fundamental = security.Fundamentals
-			self.MarketCap = fundamental.MarketCap
+			if not fundamental:
+				continue
+			
+			self.MarketCap = fundamental.MarketCap / 1_000_000_000
 			
 			# Calculate the new market cap (NMC)
-			self.NMC = round((self.MarketCap / 1_000_000_000) ** 0.5, 2)
+			self.NMC = round((self.MarketCap) ** self.power_factor, 2)
 			
 			# Get the volume for the current second (Vsec)
 			current_second = self.Time.second
-			self.Vsec = self.ActiveSecurities[symbol].Volume - self.volume_last_second.get(symbol, 0)
-			self.volume_last_second[symbol] = self.ActiveSecurities[symbol].Volume
+			self.Vsec = self.V - self.volume_last_second.get(symbol, 0)
+			self.volume_last_second[symbol] = self.V
 			
 			# Get the exact time interval of the last "second" (Vt)
-			self.V = self.Time - timedelta(seconds=current_second)
+			self.Vt = self.Time - timedelta(seconds=current_second)
 			
 			# Calculate Pr based on your reference time (e.g., 11:30 AM)
 			self.Tr = self.Time.replace(hour=11, minute=30, second=0, microsecond=0)
@@ -134,8 +141,46 @@ class ScannerAlgorithm(QCAlgorithm):
 			# Store the current Pr for the next iteration
 			self.pr[symbol] = self.P
 			
+			if not self.Pr > 0:
+				continue
+			
+			# Calculate Vmin
+			self.Vmin = self.Vsec / self.Vt.timestamp() * 60
+			
+			# Calculate VPnow
+			self.VPnow = self.Vmin * self.P * self.A / 1000
+			
+			# Calculate VPold
+			self.VPold = self.Pr * self.Hvol / 1000
+			
+			# Calculate MinDecline and ActualDescent
+			self.MinDecline = self.min_descent / self.NMC / 10000
+			self.ActualDescent = (self.Pr - self.P) / self.Pr
+			
+			# Calculate Y and Z
+			self.Y = self.VPnow / self.NMC
+			self.Z = self.C * self.NMC * self.VPnow / self.VPold
+			
 			# Now you have all the required information for each stock
 			self.Debug(
-				f"Symbol: {symbol}, Pr: {self.Pr}, Hvol: {self.Hvol}, P: {self.P}, "
-				f"Market Cap: {self.MarketCap}, NMC: {self.NMC}, Vsec: {self.Vsec}, Vt: {self.Vt}"
-			)
+				f"Symbol: {symbol}, "
+				f"P: {self.P}, "
+				f"V: {self.V}, "
+				f"Pr: {self.Pr}, "
+				f"Hvol: {self.Hvol}, "
+				f"Market Cap: {self.MarketCap}, "
+				f"NMC: {self.NMC}, "
+				f"Vsec: {self.Vsec}, "
+				f"Vt: {self.Vt}, "
+				f"Vmin: {self.Vmin}, "
+				f"VPnow: {self.VPnow}, "
+				f"VPold: {self.VPold}, "
+				f"MinDecline: {self.MinDecline}, "
+				f"ActualDescent: {self.ActualDescent}, "
+				f"Y: {self.Y}, "
+				f"Z: {self.Z}")
+			
+			# Check if thresholds are met
+			if self.Y > self.Ya and self.Z > self.Za and actual_descent > min_decline:
+				# Place trade
+				self.Debug(f"Ticker Found: {symbol.Value}")
